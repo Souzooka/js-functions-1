74d7641e4909fe0a69d6e7f5d20e7eeef220c4bb
Dakota <bazookaharwell@gmail.com>
2017-03-13 16:56:33 -1000

    isGreaterThan

--- functions.js
+++ functions.js
@@ -127,6 +127,10 @@ function increase(num) {
  * @return {boolean} `a` is larger than `b`
  */
 
+ function isGreaterThan(a, b) {
+  return (a > b);
+ }
+
 
 /**
  * Returns true if `a` is less than `b`.
